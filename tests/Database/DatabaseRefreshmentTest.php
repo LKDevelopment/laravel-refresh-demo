deb6bd359c9d6939ddc2406b01462f2f539b088a
Lukas Kämmerling <lukas.kaemmerling@s-imk.de>
2017-02-16 19:36:11 +0100

    Start Implement Tests

--- tests/Database/DatabaseRefreshmentTest.php
+++ tests/Database/DatabaseRefreshmentTest.php
@@ -10,7 +10,7 @@ namespace LKDevelopment\LaravelRefreshDemo\Tests\Database;
 
 use LKDevelopment\LaravelRefreshDemo\Tests\TestCase;
 
-class TestDatabaseRefreshment extends TestCase
+class DatabaseRefreshmentTest extends TestCase
 {
     public function test_database_refreshment(){
         $this->assertThat(true);
